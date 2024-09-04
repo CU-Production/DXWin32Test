08de480aa1f89c9b237d67ebf9ccd32eb6e87da0
THISISAGOODNAME <yangyj19931231@gmail.com>
2024-09-04 17:25:32 +0800

    Triangle

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@
 #pragma comment(lib, "d3d11")
 #pragma comment(lib, "d3dcompiler")
 
+IDXGISwapChain* g_swapchain;
+ID3D11Device* g_dev;
+ID3D11DeviceContext* g_devcon;
+
+void InitD3D(HWND hWnd);
+void CleanD3D();
+
 LRESULT CALLBACK WindowProc(HWND hWnd,
                             UINT message,
                             WPARAM wParam,
@@ -51,6 +58,8 @@ int WINAPI WinMain(HINSTANCE hInstance,
 
     ShowWindow(hWnd, nShowCmd);
 
+    InitD3D(hWnd);
+
     MSG msg = {0};
 
     while (TRUE)
@@ -69,6 +78,7 @@ int WINAPI WinMain(HINSTANCE hInstance,
         }
     }
 
+    CleanD3D();
 
     return msg.wParam;
 }
@@ -85,4 +95,37 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lPara
     }
 
     return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+void InitD3D(HWND hWnd)
+{
+    DXGI_SWAP_CHAIN_DESC scd;
+    ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+    scd.BufferCount = 1;                                    // one back buffer
+    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;     // use 32-bit color
+    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;      // how swap chain is to be used
+    scd.OutputWindow = hWnd;                                // the window to be used
+    scd.SampleDesc.Count = 4;                               // how many multisamples
+    scd.Windowed = TRUE;                                    // windowed/full-screen mode
+
+    D3D11CreateDeviceAndSwapChain(NULL,
+                                  D3D_DRIVER_TYPE_HARDWARE,
+                                  NULL,
+                                  NULL,
+                                  NULL,
+                                  NULL,
+                                  D3D11_SDK_VERSION,
+                                  &scd,
+                                  &g_swapchain,
+                                  &g_dev,
+                                  NULL,
+                                  &g_devcon);
+}
+
+void CleanD3D()
+{
+    g_swapchain->Release();
+    g_dev->Release();
+    g_devcon->Release();
 }
